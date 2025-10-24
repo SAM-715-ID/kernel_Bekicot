68c9cdd4705275c034c10a7c4cd961aceb66bc2f
Robin Gong <yibin.gong@nxp.com>
2025-10-24 13:53:20 +0800

    spi: imx: keep dma request disabled before dma transfer setup
    [ Upstream commit 86d57d9c07d54e8cb385ffe800930816ccdba0c1 ]
    
    Since sdma hardware configure postpone to transfer phase, have to disable
    dma request before dma transfer setup because there is a hardware
    limitation on sdma event enable(ENBLn) as below:
    
    "It is thus essential for the Arm platform to program them before any DMA
     request is triggered to the SDMA, otherwise an unpredictable combination
     of channels may be started."
    
    Signed-off-by: Carlos Song <carlos.song@nxp.com>
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>
    Link: https://patch.msgid.link/20251024055320.408482-1-carlos.song@nxp.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- drivers/spi/spi-imx.c
+++ drivers/spi/spi-imx.c
@@ -447,9 +447,15 @@ static void mx51_ecspi_trigger(struct spi_imx_data *spi_imx)
 {
 	u32 reg;
 
-	reg = readl(spi_imx->base + MX51_ECSPI_CTRL);
-	reg |= MX51_ECSPI_CTRL_XCH;
-	writel(reg, spi_imx->base + MX51_ECSPI_CTRL);
+	if (spi_imx->usedma) {
+		reg = readl(spi_imx->base + MX51_ECSPI_DMA);
+		reg |= MX51_ECSPI_DMA_TEDEN | MX51_ECSPI_DMA_RXDEN;
+		writel(reg, spi_imx->base + MX51_ECSPI_DMA);
+	} else {
+		reg = readl(spi_imx->base + MX51_ECSPI_CTRL);
+		reg |= MX51_ECSPI_CTRL_XCH;
+		writel(reg, spi_imx->base + MX51_ECSPI_CTRL);
+	}
 }
 
 static int mx51_ecspi_prepare_message(struct spi_imx_data *spi_imx,
@@ -561,7 +567,6 @@ static int mx51_ecspi_config(struct spi_device *spi)
 	writel(MX51_ECSPI_DMA_RX_WML(spi_imx->wml) |
 		MX51_ECSPI_DMA_TX_WML(spi_imx->wml) |
 		MX51_ECSPI_DMA_RXT_WML(spi_imx->wml) |
-		MX51_ECSPI_DMA_TEDEN | MX51_ECSPI_DMA_RXDEN |
 		MX51_ECSPI_DMA_RXTDEN, spi_imx->base + MX51_ECSPI_DMA);
 
 	return 0;
@@ -1263,6 +1268,8 @@ static int spi_imx_dma_transfer(struct spi_imx_data *spi_imx,
 	dmaengine_submit(desc_tx);
 	reinit_completion(&spi_imx->dma_tx_completion);
 	dma_async_issue_pending(master->dma_tx);
+
+	spi_imx->devtype_data->trigger(spi_imx);
 
 	transfer_timeout = spi_imx_calculate_timeout(spi_imx, transfer->len);
 
