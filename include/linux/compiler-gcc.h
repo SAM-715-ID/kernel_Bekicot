661c05a2f0429f35e380014e928ecbba209d04c8
KeongBalap <artiyailbaraka@gmail.com>
2026-02-12 14:37:31 +0700

    Revert "kmsan: introduce __no_sanitize_memory and __no_kmsan_checks"
    This reverts commit 0e4392bbb5989ba3255b45a4c19d96321234d9eb.

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -390,12 +390,6 @@
 #endif
 
 /*
- * GCC does not support KMSAN.
- */
-#define __no_sanitize_memory
-#define __no_kmsan_checks
-
-/*
  * Turn individual warnings and errors on and off locally, depending
  * on version.
  */
