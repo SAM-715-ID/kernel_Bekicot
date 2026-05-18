945d3c97f078c4236ca3d947990133833550e5f8
Seungjin Bae <eeodqql09@gmail.com>
2026-05-18 19:43:14 -0400

    usb: gadget: dummy_hcd: Reject hub port requests for non-existent ports
    commit 7d9633528dd40e33964d2dc74a5abbf5c4d116ce upstream.
    
    The `dummy_hub_control()` function handles USB hub class requests
    to the virtual root hub. The `GetPortStatus` case returns -EPIPE for
    requests with `wIndex != 1`, since the virtual root hub has only a
    single port. However, the `ClearPortFeature` and `SetPortFeature`
    cases lack the same check.
    
    Fix this by extending the `wIndex != 1` rejection to both cases,
    matching the existing behavior of `GetPortStatus`.
    
    Fixes: 1da177e4c3f4 ("Linux-2.6.12-rc2")
    Cc: stable <stable@kernel.org>
    Suggested-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Seungjin Bae <eeodqql09@gmail.com>
    Reviewed-by: Alan Stern <stern@rowland.harvard.edu>
    Link: https://patch.msgid.link/20260518234314.1889396-1-eeodqql09@gmail.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- drivers/usb/gadget/udc/dummy_hcd.c
+++ drivers/usb/gadget/udc/dummy_hcd.c
@@ -2135,6 +2135,8 @@ static int dummy_hub_control(
 	case ClearHubFeature:
 		break;
 	case ClearPortFeature:
+		if (wIndex != 1)
+			goto error;
 		switch (wValue) {
 		case USB_PORT_FEAT_SUSPEND:
 			if (hcd->speed == HCD_USB3) {
@@ -2239,6 +2241,8 @@ static int dummy_hub_control(
 		retval = -EPIPE;
 		break;
 	case SetPortFeature:
+		if (wIndex != 1)
+			goto error;
 		switch (wValue) {
 		case USB_PORT_FEAT_LINK_STATE:
 			if (hcd->speed != HCD_USB3) {
