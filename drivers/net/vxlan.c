a449348cf24f495b24112555be5e873f3951434f
Eric Dumazet <edumazet@google.com>
2026-05-25 20:36:42 +0000

    vxlan: do not reuse cached ip_hdr() value after skb_tunnel_check_pmtu()
    [ Upstream commit 7d9ef0cb271555d8cf39fefe6c981e1493b25ecf ]
    
    skb_tunnel_check_pmtu() can change skb->head.
    
    Reusing old_iph afer skb_tunnel_check_pmtu() can cause an UAF.
    
    Use instead ip_hdr(skb) as done in drivers/net/bareudp.c
    and drivers/net/geneve.c.
    
    Found by Sashiko.
    
    Fixes: 4cb47a8644cc ("tunnels: PMTU discovery support for directly bridged IP packets")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Stefano Brivio <sbrivio@redhat.com>
    Link: https://patch.msgid.link/20260525203642.2389723-1-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Ulrich Hecht <uli@kernel.org>

--- drivers/net/vxlan.c
+++ drivers/net/vxlan.c
@@ -2237,7 +2237,7 @@ static void vxlan_xmit_one(struct sk_buff *skb, struct net_device *dev,
 			skb_dst_update_pmtu(skb, mtu);
 		}
 
-		tos = ip_tunnel_ecn_encap(tos, old_iph, skb);
+		tos = ip_tunnel_ecn_encap(tos, ip_hdr(skb), skb);
 		ttl = ttl ? : ip4_dst_hoplimit(&rt->dst);
 		err = vxlan_build_skb(skb, ndst, sizeof(struct iphdr),
 				      vni, md, flags, udp_sum);
@@ -2278,7 +2278,7 @@ static void vxlan_xmit_one(struct sk_buff *skb, struct net_device *dev,
 			skb_dst_update_pmtu(skb, mtu);
 		}
 
-		tos = ip_tunnel_ecn_encap(tos, old_iph, skb);
+		tos = ip_tunnel_ecn_encap(tos, ip_hdr(skb), skb);
 		ttl = ttl ? : ip6_dst_hoplimit(ndst);
 		skb_scrub_packet(skb, xnet);
 		err = vxlan_build_skb(skb, ndst, sizeof(struct ipv6hdr),
